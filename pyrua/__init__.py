0a8d0d4d0833443536930bd84774cd9313ef867f
farhanaliofficial <officialofun@gmail.com>
2025-12-04 16:36:59 +0500

    Update GitHub URL in __init__.py

--- pyrua/__init__.py
+++ pyrua/__init__.py
@@ -2,5 +2,5 @@ from .pyrua import get_rua
 
 __author__ = "Farhan Ali"
 __version__ = "0.1"
-__github__ = "https://github.com/farhanaliofficial/pyrua"
+__github__ = "https://github.com/farhaanaliii/pyrua"
 __all__ = ['get_rua']
